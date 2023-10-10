74d12fe31eb204dcd036250eefc196e2cc2437ce
Hynth <cdar93@gmail.com>
2023-10-10 09:58:11 -0500

    solved task1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,20 +6,10 @@ import ShowContacts from "./ShowContacts";
 export const App = () => {
   
   const LOCALSTORAGE_KEY = "phonebook-state";
-  const data = [
-    {id: 'id-1', name: 'Rosie Simpson', number: '459-12-56'},
-    {id: 'id-2', name: 'Hermione Kline', number: '443-89-12'},
-    {id: 'id-3', name: 'Eden Clements', number: '645-17-79'},
-    {id: 'id-4', name: 'Annie Copeland', number: '227-91-26'}
-  ];
-
-  const storage = JSON.parse(localStorage.getItem(LOCALSTORAGE_KEY));
-  if(!storage) {
-    localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(data));
-  }
+  const storedContacts = JSON.parse(localStorage.getItem(LOCALSTORAGE_KEY));
 
   const [state, setState] = useState({
-    contacts: [],
+    contacts: storedContacts,
     filter: '',
     name: '',
     number: ''
@@ -28,9 +18,9 @@ export const App = () => {
   const [showContact, setShowContact] = useState(state.contacts);
 
   useEffect(() => {
-    const data = JSON.parse(localStorage.getItem(LOCALSTORAGE_KEY));
-    setState({...state, contacts: data});
-  }, []);  
+    localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(state.contacts));
+    
+  }, [state]);  
 
 
   return (
